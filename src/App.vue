51dc253c78c747959a9a987703d2d87efe794d7c
dingming <ddd>
2023-07-06 17:47:47 +0800

    14-vue通用后台管理（菜单样式与less引入）

--- src/App.vue
+++ src/App.vue
@@ -8,3 +8,10 @@
 #app {
 }
 </style>
+
+<style>
+html,body{
+  margin: 0;
+  padding: 0;
+}
+</style>
--- src/components/aside.vue
+++ src/components/aside.vue
@@ -4,7 +4,8 @@
         <el-radio-button :label="false">展开</el-radio-button>
         <el-radio-button :label="true">收起</el-radio-button>
     </el-radio-group> -->
-    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse" >
+    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+        <h3>通用后台管理</h3>
         <el-menu-item v-for="item in noChildren" :key="item.name" :index="item.name">
             <i :class="`el-icon-${item.icon}`"></i>
             <span slot="title">{{item.label}}</span>
@@ -14,8 +15,8 @@
                 <i :class="`el-icon-${item.icon}`"></i>
                 <span slot="title">{{item.label}}</span>
             </template>
-            <el-menu-item-group >
-                <el-menu-item  v-for="subItem in item.children" :index="item.url">{{subItem.label}}</el-menu-item>
+            <el-menu-item-group>
+                <el-menu-item v-for="subItem in item.children" :index="item.url">{{subItem.label}}</el-menu-item>
             </el-menu-item-group>
         </el-submenu>
 
@@ -23,10 +24,20 @@
 </div>
 </template>
 
-<style>
+<style lang="less" scoped>
 .el-menu-vertical-demo:not(.el-menu--collapse) {
     width: 200px;
     min-height: 400px;
+}
+.el-menu{
+    height: 100vh;
+    h3{
+        color:#fff;
+        text-align: center;
+        line-height: 48px;
+        font-size: 16px;
+        font-weight: 400;
+    }
 }
 </style>
 
@@ -86,12 +97,12 @@ export default {
             console.log(key, keyPath);
         }
     },
-    computed:{
-        noChildren(){
-            return this.menuData.filter(item=>!item.children)
+    computed: {
+        noChildren() {
+            return this.menuData.filter(item => !item.children)
         },
-        hasChildren(){
-            return this.menuData.filter(item=>item.children)
+        hasChildren() {
+            return this.menuData.filter(item => item.children)
         }
     }
 }
