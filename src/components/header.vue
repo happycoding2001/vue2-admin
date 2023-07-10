f60eca86337e07deb35878de89c4378bc7aaf720
dingming <ddd>
2023-07-10 15:02:06 +0800

    点击header上的标签会报错redundant navigation to current location

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="header-container">
     <div class="l-content">
-        <el-button @click="handelMenu" icon="el-icon-menu" size="mini"></el-button>
+        <el-button style="margin-right: 20px;" @click="handelMenu" icon="el-icon-menu" size="mini"></el-button>
         <!-- 面包屑 -->
         <el-breadcrumb separator="/">
             <el-breadcrumb-item v-for="item in tags" :key="item.path" :to="{ path: item.path }">{{item.label}}</el-breadcrumb-item>
@@ -68,6 +68,23 @@ export default {
             width: 40px;
             height: 40px;
             border-radius: 50%;
+        }
+    }
+    .l-content{
+        display: flex;
+        align-items: center;
+        /deep/.el-breadcrumb__item{
+            .el-breadcrumb__inner{
+                font-weight: normal;
+                &.is-link{
+                    color: #666;
+                }
+            }
+            &:last-child{
+                .el-breadcrumb__inner{
+                    color: #fff;
+                }
+            }
         }
     }
 }
