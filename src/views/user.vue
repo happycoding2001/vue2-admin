d7de3ab46499e7c5d79f05dec10f3fe5f8d2bcfa
dingming <ddd>
2023-07-11 10:48:43 +0800

    36-vue通用后台管理（用户管理页面功能form页面编写）

--- src/views/user.vue
+++ src/views/user.vue
@@ -1,15 +1,85 @@
 <template>
 <div>
-    user
+    <div class="manage">
+        <el-dialog title="提示" :visible.sync="dialogVisible" width="50%">
+            <el-form :inline="true" ref="form" :model="form" label-width="80px">
+                <el-form-item label="姓名">
+                    <el-input placeholder="请输入姓名" v-model="form.name"></el-input>
+                </el-form-item>
+                <el-form-item label="年龄">
+                    <el-input placeholder="请输入年龄" v-model="form.age"></el-input>
+                </el-form-item>
+                <el-form-item label="性别">
+                    <el-select v-model="form.sex" placeholder="请选择">
+                        <el-option label="男" value="1"></el-option>
+                        <el-option label="女" value="0"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="出生日期">
+                    <el-date-picker v-model="form.birth" type="date" placeholder="选择日期时间">
+                    </el-date-picker>
+                </el-form-item>
+                <el-form-item label="地址">
+                    <el-input placeholder="请输入地址" v-model="form.addr"></el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+            </span>
+        </el-dialog>
+        <div class="manage-head">
+            <el-button @click="dialogVisible=true" type="primary">+ 新增</el-button>
+        </div>
+
+    </div>
 </div>
 </template>
 
 <script>
+// {
+//     model: 'name',
+//     label: '姓名',
+//     type: 'input'
+// },
+// {
+//     model: 'age',
+//     label: '年龄',
+//     type: 'input'
+// }, {
+//     model: 'sex',
+//     label: '性别',
+//     type: 'select',
+//     opts: [{
+//             label: '男',
+//             value: 1
+//         },
+//         {
+//             label: '女',
+//             value: 0
+//         }
+//     ]
+// }, {
+//     model: 'birth',
+//     label: '出生日期',
+//     type: 'date'
+// }, {
+//     model: 'addr',
+//     label: '地址',
+//     type: 'input'
+// }
 export default {
     name: '',
     data() {
         return {
-
+            dialogVisible: false,
+            form: {
+                name: '',
+                age: '',
+                sex: '',
+                birth: '',
+                addr: '',
+            }
         }
     }
 }
