e939b078f7e9bd6a40f8d4d78e73d77581afbfac
dingming <ddd>
2023-07-11 20:24:08 +0800

    save

--- src/views/user.vue
+++ src/views/user.vue
@@ -1,59 +1,61 @@
 <template>
-
-    <div style="height: 90%;" class="manage">
-        <el-dialog title="提示" :visible.sync="dialogVisible" width="50%" :before-close="handelClose">
-            <el-form :inline="true" ref="form" :rules="rules" :model="form" label-width="80px">
-                <el-form-item prop="name" label="姓名">
-                    <el-input placeholder="请输入姓名" v-model="form.name"></el-input>
-                </el-form-item>
-                <el-form-item prop="age" label="年龄">
-                    <el-input placeholder="请输入年龄" v-model="form.age"></el-input>
-                </el-form-item>
-                <el-form-item prop="sex" label="性别">
-                    <el-select v-model="form.sex" placeholder="请选择">
-                        <el-option label="男" :value="1"></el-option>
-                        <el-option label="女" :value="0"></el-option>
-                    </el-select>
-                </el-form-item>
-                <el-form-item prop="birth" label="出生日期">
-                    <el-date-picker value-format="yyyy-MM-dd" v-model="form.birth" type="date" placeholder="选择日期时间">
-                    </el-date-picker>
-                </el-form-item>
-                <el-form-item prop="addr" label="地址">
-                    <el-input placeholder="请输入地址" v-model="form.addr"></el-input>
-                </el-form-item>
-            </el-form>
-            <span slot="footer" class="dialog-footer">
-                <el-button @click="cancel">取 消</el-button>
-                <el-button type="primary" @click="submit">确 定</el-button>
-            </span>
-        </el-dialog>
-        <div class="manage-header">
-            <el-button @click="handleAdd" type="primary">+ 新增</el-button>
-        </div>
-        <el-table height="90%" :data="tableData" style="width: 100%">
-            <el-table-column prop="name" label="姓名">
-            </el-table-column>
-            <el-table-column prop="sex" label="性别">
-                <template slot-scope="scope">
-                    <span>{{ scope.row.sex === 1 ? '男' : '女' }}</span>
-                </template>
-            </el-table-column>
-            <el-table-column prop="age" label="年龄">
-            </el-table-column>
-            <el-table-column prop="birth" label="出生日期">
-            </el-table-column>
-            <el-table-column prop="addr" label="地址">
-            </el-table-column>
-            <el-table-column prop="edit" label="操作">
-                <template slot-scope="scope">
-                    <el-button size="mini" @click="handleEdit(scope.row)">编辑</el-button>
-                    <el-button size="mini" type="danger" @click="handleDel(scope.row)">删除</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
+<div style="height: 90%;" class="manage">
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="50%" :before-close="handelClose">
+        <el-form :inline="true" ref="form" :rules="rules" :model="form" label-width="80px">
+            <el-form-item prop="name" label="姓名">
+                <el-input placeholder="请输入姓名" v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item prop="age" label="年龄">
+                <el-input placeholder="请输入年龄" v-model="form.age"></el-input>
+            </el-form-item>
+            <el-form-item prop="sex" label="性别">
+                <el-select v-model="form.sex" placeholder="请选择">
+                    <el-option label="男" :value="1"></el-option>
+                    <el-option label="女" :value="0"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item prop="birth" label="出生日期">
+                <el-date-picker value-format="yyyy-MM-dd" v-model="form.birth" type="date" placeholder="选择日期时间">
+                </el-date-picker>
+            </el-form-item>
+            <el-form-item prop="addr" label="地址">
+                <el-input placeholder="请输入地址" v-model="form.addr"></el-input>
+            </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="cancel">取 消</el-button>
+            <el-button type="primary" @click="submit">确 定</el-button>
+        </span>
+    </el-dialog>
+    <div class="manage-header">
+        <el-button @click="handleAdd" type="primary">+ 新增</el-button>
     </div>
-
+    <el-table height="90%" :data="tableData" style="width: 100%">
+        <el-table-column prop="name" label="姓名">
+        </el-table-column>
+        <el-table-column prop="sex" label="性别">
+            <template slot-scope="scope">
+                <span>{{ scope.row.sex === 1 ? '男' : '女' }}</span>
+            </template>
+        </el-table-column>
+        <el-table-column prop="age" label="年龄">
+        </el-table-column>
+        <el-table-column prop="birth" label="出生日期">
+        </el-table-column>
+        <el-table-column prop="addr" label="地址">
+        </el-table-column>
+        <el-table-column prop="edit" label="操作">
+            <template slot-scope="scope">
+                <el-button size="mini" @click="handleEdit(scope.row)">编辑</el-button>
+                <el-button size="mini" type="danger" @click="handleDel(scope.row)">删除</el-button>
+            </template>
+        </el-table-column>
+    </el-table>
+    <div>
+        <el-pagination @current-change="handlePageChange" layout="prev, pager, next" :total="total">
+        </el-pagination>
+    </div>
+</div>
 </template>
 
 <script>
@@ -66,6 +68,11 @@ export default {
     name: '',
     data() {
         return {
+            pageData:{
+                pageIndex:1,
+                pageSize:10,
+            },
+            total:0,
             modalType: 0, // 0 新增   1 编辑
             dialogVisible: false,
             form: {
@@ -101,6 +108,10 @@ export default {
         }
     },
     methods: {
+        handlePageChange(val){
+            this.pageData.pageIndex = val
+            this.getList()
+        },
         handleAdd() {
             this.modalType = 0
             this.dialogVisible = true
@@ -111,7 +122,9 @@ export default {
                 cancelButtonText: '取消',
                 type: 'warning'
             }).then(() => {
-                delUser({id:row.id}).then(() => {
+                delUser({
+                    id: row.id
+                }).then(() => {
                     this.$message({
                         type: 'success',
                         message: '删除成功!'
@@ -164,8 +177,15 @@ export default {
             this.dialogVisible = false
         },
         getList() {
-            getUser().then(res => {
+            let {pageIndex,pageSize} = this.pageData
+            let params = {
+                page:pageIndex,
+                limit:pageSize
+            }
+            
+            getUser({params}).then(res => {
                 this.tableData = res.data.list
+                this.total = res.data.count ? res.data.count : 0
             })
         },
     },
@@ -176,7 +196,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.manage{
-    
-}
+.manage {}
 </style>
