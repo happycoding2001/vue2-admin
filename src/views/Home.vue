95fecf690dea8ba43d8f69404aaac6c628d0546c
dingming <ddd>
2023-07-28 17:00:08 +0800

    递归

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,340 +1,98 @@
 <template>
-<div>
-    <el-row>
-        <el-col :span="8" style="padding-right: 10px;">
-            <el-card class="box-card">
-                <div class="user">
-                    <img src="../assets/images/user.png" alt="">
-                    <div class="userinfo">
-                        <p class="name">Admin</p>
-                        <p class="access">超级管理员</p>
-                    </div>
-                </div>
-                <div class="login-info">
-                    <p>上次登录时间：<span>2021-7-19</span></p>
-                    <p>上次登录地点：<span>武汉</span></p>
-                </div>
-            </el-card>
-            <el-card style="margin-top: 20px;height: 460px;">
-                <el-table :data="tableData" style="width: 100%">
-                    <el-table-column v-for="(val,key) in tableLabel" :prop="key" :label="val">
-                    </el-table-column>
 
-                </el-table>
-            </el-card>
-        </el-col>
-        <el-col :span="16" style="padding-left: 10px;">
-            <div class="num">
-                <el-card v-for="item in countData" :key="item.name" :body-style="{display:'flex',padding:0}">
-                    <i :style="{background:item.color}" class="icon" :class="`el-icon-${item.icon}`"></i>
-                    <div class="detail">
-                        <p class="price">￥{{item.value}}</p>
-                        <p class="desc">{{item.name}}</p>
-                    </div>
-                </el-card>
-            </div>
-            <el-card style="height: 280px;">
-                <!-- 折线图 -->
-                <div style="height: 260px;" ref="echarts1"></div>
-            </el-card>
-            <div class="graph">
-                <el-card style="height: 260px;">
-                    <div style="height: 260px;" ref="echarts2"></div>
-                </el-card>
-                <el-card style="height: 260px;">
-                    <div style="height: 240px;" ref="echarts3"></div>
-                </el-card>
-            </div>
-        </el-col>
-    </el-row>
-</div>
 </template>
 
 <script>
-import {
-    getData
-} from '../api';
-import * as echarts from 'echarts';
-
 export default {
     name: '',
     data() {
         return {
-            tableData: [],
-            tableLabel: {
-                name: '课程',
-                todayBuy: '今日购买',
-                monthBuy: '本月购买',
-                totalBuy: '总购买',
-            },
-            countData: [{
-                    name: "今日支付订单",
-                    value: 1234,
-                    icon: "success",
-                    color: "#2ec7c9",
+            list: [{
+                    dictvalue: 1,
+                    dictLabel: "a1",
+                    children: [{
+                            dictvalue: 4,
+                            dictLabel: "a2",
+                        },
+                        {
+                            dictvalue: 5,
+                            dictLabel: "a3",
+                            children: [{
+                                    dictvalue: 7,
+                                    dictLabel: "a4",
+                                },
+                                {
+                                    dictvalue: 8,
+                                    dictLabel: "a5",
+                                    children: [{
+                                            dictvalue: 11,
+                                            dictLabel: "a6",
+                                        },
+                                        {
+                                            dictvalue: 10,
+                                            dictLabel: "a7",
+                                        },
+                                    ],
+                                },
+                                {
+                                    dictvalue: 9,
+                                    dictLabel: "a8",
+                                },
+                            ],
+                        },
+                    ],
                 },
                 {
-                    name: "今日收藏订单",
-                    value: 210,
-                    icon: "star-on",
-                    color: "#ffb980",
+                    dictvalue: 2,
+                    dictLabel: "a9",
                 },
                 {
-                    name: "今日未支付订单",
-                    value: 1234,
-                    icon: "s-goods",
-                    color: "#5ab1ef",
+                    dictvalue: 3,
+                    dictLabel: "a10",
+                    children: [{
+                        dictvalue: 6,
+                        dictLabel: "a11",
+                        children: [{
+                            dictvalue: 12,
+                            dictLabel: "a12",
+                        }, ],
+                    }, ],
                 },
-                {
-                    name: "本月支付订单",
-                    value: 1234,
-                    icon: "success",
-                    color: "#2ec7c9",
-                },
-                {
-                    name: "本月收藏订单",
-                    value: 210,
-                    icon: "star-on",
-                    color: "#ffb980",
-                },
-                {
-                    name: "本月未支付订单",
-                    value: 1234,
-                    icon: "s-goods",
-                    color: "#5ab1ef",
-                },
-            ],
+            ]
         }
     },
-    mounted() {
-        getData().then(res => {
-            console.log(res.data.data)
-            const tableData = res.data.data.tableData;
-            this.tableData = tableData
+    methods: {
+        getTreeName(list, id) {
+            let _this = this;
+            let str = "";
+            for (let i = 0; i < list.length; i++) {
+                let a = list[i];
 
-            const echarts1 = echarts.init(this.$refs.echarts1)
-
-            let option1 = {
-                yAxis: {}
+                if (a.dictvalue === id) {
+                    str += a.dictLabel
+                    return str;
+                } else {
+                    if (a.children && a.children.length > 0) {
+                        str += a.dictLabel + "/";
+                        let res = _this.getTreeName(a.children, id);
+                        if (res) {
+                            
+                            str += res
+                            return str;
+                        }
+                    }
+                }
             }
-            const {
-                orderData,
-                userData,
-                videoData,
-            } = res.data.data
-
-            const xAxis = Object.keys(orderData.data[0])
-            option1.xAxis = {
-                data: xAxis
-            }
-
-            option1.legend = {
-                data: xAxis
-            }
-            option1.series = []
-            xAxis.forEach(key => {
-                option1.series.push({
-                    name: key,
-                    data: orderData.data.map(item => item[key]),
-                    type: 'line',
-                })
-            })
-            echarts1.setOption(option1)
-            console.log(userData.map(item => item.new))
-            // 柱状图
-            const echarts2 = echarts.init(this.$refs.echarts2)
-            let option2 = {
-                legend: {
-                    // 图例文字颜色
-                    textStyle: {
-                        color: "#333",
-                    },
-                },
-                grid: {
-                    left: "20%",
-                },
-                // 提示框
-                tooltip: {
-                    trigger: "axis",
-                },
-                xAxis: {
-                    type: "category", // 类目轴
-                    data: userData.map(item => item.date),
-                    axisLine: {
-                        lineStyle: {
-                            color: "#17b3a3",
-                        },
-                    },
-                    axisLabel: {
-                        interval: 0,
-                        color: "#333",
-                    },
-                },
-                yAxis: [{
-                    type: "value",
-                    axisLine: {
-                        lineStyle: {
-                            color: "#17b3a3",
-                        },
-                    },
-                }, ],
-                color: ["#2ec7c9", "#b6a2de", "#5ab1ef", "#ffb980", "#d87a80", "#8d98b3"],
-                series: [{
-                        name: '新增用户',
-                        data: userData.map(item => item.new),
-                        type: 'bar'
-                    },
-                    {
-                        name: '活跃用户',
-                        data: userData.map(item => item.active),
-                        type: 'bar'
-                    },
-                ],
-            }
-            
-            console.log(option2)
-            echarts2.setOption(option2)
-
-            // 饼图
-            const echarts3 = echarts.init(this.$refs.echarts3)
-            let option3 = {
-                legend: {
-                    // 图例文字颜色
-                    textStyle: {
-                        color: "#333",
-                    },
-                },
-                grid: {
-                    left: "20%",
-                },
-                // 提示框
-                tooltip: {
-                    trigger: "axis",
-                },
-                xAxis: {
-                    type: "category", // 类目轴
-                    data: [],
-                    axisLine: {
-                        lineStyle: {
-                            color: "#17b3a3",
-                        },
-                    },
-                    axisLabel: {
-                        interval: 0,
-                        color: "#333",
-                    },
-                },
-                yAxis: [{
-                    type: "value",
-                    axisLine: {
-                        lineStyle: {
-                            color: "#17b3a3",
-                        },
-                    },
-                }, ],
-                color: ["#2ec7c9", "#b6a2de"],
-                series: [{
-                    data:videoData,
-                    type:'pie',
-                }],
-            }
-            echarts3.setOption(option3)
-        })
-
+        }
+    },
+    mounted(){
+        
+       let res = this.getTreeName(this.list,12)
+       console.log(res)
     }
 }
 </script>
 
-<style lang="scss" scoped>
-.user {
-    display: flex;
-    align-items: center;
-    padding-bottom: 20px;
-    margin-bottom: 20px;
-    border-bottom: 1px solid #ccc;
+<style lang="" scoped>
 
-    img {
-        width: 150px;
-        height: 150px;
-        border-radius: 50%;
-        margin-right: 40px;
-    }
-
-    .userinfo {
-        .name {
-            font-size: 32px;
-            margin-bottom: 10px;
-        }
-
-        .access {
-            color: #999;
-            // font-size: ;
-        }
-    }
-
-}
-
-.login-info {
-    p {
-        line-height: 28px;
-        font-size: 14px;
-        color: #999;
-
-        span {
-            color: #666;
-            margin-right: 60px;
-        }
-    }
-}
-
-.num {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-
-    .icon {
-        width: 80px;
-        height: 80px;
-        font-size: 30px;
-        text-align: center;
-        line-height: 80px;
-        color: #fff;
-    }
-
-    .detail {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        margin-left: 15px;
-
-        .price {
-            font-size: 30px;
-            margin-bottom: 10px;
-            line-height: 30px;
-            height: 30px;
-        }
-
-        .desc {
-            font-size: 14px;
-            color: #999;
-            text-align: center;
-        }
-    }
-
-    .el-card {
-        width: 32%;
-        margin-bottom: 20px;
-
-    }
-}
-
-.graph {
-    margin-top: 20px;
-    display: flex;
-    justify-content: space-between;
-
-    .el-card {
-        width: 48%;
-    }
-}
 </style>
